ff271e6cf09a1bbc46cdd331d1a5426168edd56d
Mert Pinar <merpin@ktu.lt>
2026-04-02 13:03:25 +0300

    I created the User Dtos part and User Requests parts and also I diid some modification for category Dto

---
+++ DTOs/UserDto/UserBaseDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudyBuddy.API.DTOs
+{
+    public class UserBaseDto
+    {
+        [Table("Users")]
+        public class User
+        {
+            public string NameSurname { get; set; }
+            public string Email { get; set; }
+            public string AboutMe { get; set; }
+            public string University { get; set; }
+            public string Major { get; set; }
+        }
+    }
+}
