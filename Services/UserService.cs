3fa48cb86d676178454e6904e792c218d47af54d
Mert Pinar <mertpinar92@gmail.com>
2026-04-14 23:16:40 +0300

    STUD-21 User Business Logic implemented IUserService and also UserService part

---
+++ Services/UserService.cs
@@ -0,0 +1,42 @@
+﻿using StudyBuddy.API.Model;
+
+using StudyBuddy.API.Repository.Interface;
+using StudyBuddy.API.Services.Interface;
+
+namespace StudyBuddy.API.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public async Task<bool> DeleteUser(int id)
+        {
+            return await _userRepository.DeleteUser(id);
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _userRepository.GetAllUsers();
+        }
+
+        public async Task<User> GetUserById(int id)
+        {
+            return await _userRepository.GetUserById(id);
+        }
+
+
+        public async Task<int> InsertUser(User user)
+        {
+            return await _userRepository.InsertUser(user);
+        }
+
+        public async Task<bool> UpdateUser(User user)
+        {
+            return await _userRepository.UpdateUser(user);
+        }
+    }
+}
