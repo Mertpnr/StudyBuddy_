dfa43b807f28f3143178f1cd8c59e93ec1e5adc3
kamile-p <kamileper10@gmail.com>
2026-04-16 22:27:05 +0300

    STUD-37: Added MatchService and interface IMatchService

--- Services/Interfaces/IMatchService.cs
+++ Services/Interfaces/IMatchService.cs
@@ -1,6 +1,15 @@
-﻿namespace StudyBuddy.Services.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using StudyBuddy.API.Model;
+
+namespace StudyBuddy.API.Services.Interface
 {
-	public class IMatchService
+	public interface IMatchService
 	{
+		Task<List<Match>> GetAllMatches();
+		Task<Match> GetMatchById(int id);
+		Task<int> InsertMatch(Match match);
+		Task<bool> UpdateMatch(Match match);
+		Task<bool> DeleteMatch(int id);
 	}
 }
