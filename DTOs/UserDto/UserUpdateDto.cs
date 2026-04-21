5b9bab553642fe9d91427180061a635c6fd10247
Zabil Islamli <zabisl@ktu.lt>
2026-04-21 16:19:35 +0300

    STUD-53 User Identity DTOs: Revisited DTO classes. 3 DTO classes will inherit from UserBase

--- DTOs/UserDto/UserUpdateDto.cs
+++ DTOs/UserDto/UserUpdateDto.cs
@@ -1,9 +1,8 @@
-﻿namespace StudyBuddy.API.DTOs
+﻿namespace StudyBuddy.API.DTOs.UserDto
 {
     public class UserUpdateDto : UserBaseDto
     {
         public int UserId { get; set; }
-
-        public DateTime UpdatedDate { get; set; }
     }
 }
+
