67315a9a3a8133825dc9b6415b82fe174ebc5ba3
kamile-p <kamileper10@gmail.com>
2026-05-06 12:49:51 +0300

    STUD-79 Match and MatchRequest ViewModels. Created Match Model classes and Match Request classes

---
+++ StudyBuddy.WEB/Models/Match/MatchResultViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace StudyBuddy.WEB.Models.Match
+{
+	public class MatchResultViewModel
+	{
+		public int User1Id { get; set; }
+
+		public int User2Id { get; set; }
+
+		public decimal MatchPercent { get; set; }
+
+		public string Message { get; set; } = string.Empty;
+	}
+}
