156513eed7343fd63df12739e3563a0b183d406a
kamile-p <kamileper10@gmail.com>
2026-05-06 13:19:47 +0300

    STUD-81 Authentication and User Web Services. Added the classes and interfaces

---
+++ StudyBuddy.WEB/Services/Interfaces/IAuthWebService.cs
@@ -0,0 +1,11 @@
+﻿using StudyBuddy.WEB.Models.Auth;
+
+namespace StudyBuddy.WEB.Services.Interfaces
+{
+	public interface IAuthWebService
+	{
+		Task<bool> RegisterAsync(RegisterViewModel model);
+
+		Task<LoginResponseViewModel?> LoginAsync(LoginViewModel model);
+	}
+}
