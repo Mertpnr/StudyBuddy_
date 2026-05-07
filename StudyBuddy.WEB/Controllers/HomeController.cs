56a30a281b2eb67f507c0b4c149315a9d9301d84
linas <linser1@ktu.lt>
2026-05-07 23:45:46 +0300

    STUD-83 Home, Auth, and Dashboard Controllers

---
+++ StudyBuddy.WEB/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudyBuddy.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
